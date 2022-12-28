af03d0a05f340831f5c7100de0a8bc1bf3d6485e
yuhang zhu <zcolley96@outlook.com>
2022-12-27 20:13:04 -0500

    implement fetch jobs and add single job via reducer

--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -6,7 +6,13 @@ const reducer = (state, action) => {
   switch (action.type) {
     case "CHANGE_MODE":
       return { ...state, mode: action.payload }
-
+    case "ADD_DATA":
+      return {
+        ...state,
+        userJob: [...state.userJob, action.payload],
+      }
+    case "FETCH_DATA":
+      return { ...state, userJob: [...action.payload] }
     default:
       return state
   }
@@ -14,24 +20,25 @@ const reducer = (state, action) => {
 export function ThemeContextProvider({ children }) {
   const [state, dispatch] = useReducer(reducer, {
     mode: "dark",
+    userJob: [],
   })
-  const [userJob, setUserJobs] = useState([])
+  // const [userJob, setUserJobs] = useState([])
   const changeMode = (mode) => {
     dispatch({ type: "CHANGE_MODE", payload: mode })
   }
-  const addJob = (json) => {
-    setUserJobs([...userJob, json])
+  const addJob = (data) => {
+    dispatch({ type: "ADD_DATA", payload: data })
   }
   useEffect(() => {
     const fetchData = async () => {
       const res = await fetch("http://localhost:4000/jobs")
       const JSONdata = await res.json()
-      setUserJobs(JSONdata)
+      dispatch({ type: "FETCH_DATA", payload: JSONdata })
     }
     fetchData()
   }, [])
   return (
-    <ThemeContext.Provider value={{ ...state, changeMode, userJob, addJob }}>
+    <ThemeContext.Provider value={{ ...state, changeMode, addJob }}>
       {children}
     </ThemeContext.Provider>
   )
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,11 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { useLoaderData } from "react-router-dom"
 import Card from "../components/Card"
 import useTheme from "../hooks/useTheme"
 export default function Home() {
   const cards = useLoaderData()
   const { userJob } = useTheme()
+
   console.log(userJob)
   return (
     <div className="max-w-screen-xl	mx-auto px-3 grid  gap-y-3  grid-cols-1 md:grid-cols-3 md:gap-3">
